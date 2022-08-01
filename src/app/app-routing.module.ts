82493e26c27e33562dcb63897ca73d45ebbccba8
Sai Dheeraj Hazare <saidheeraj.hazare@valuemomentum.com>
2022-08-01 18:13:08 +0530

    committing Taskapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { HomeComponent } from './home/home.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'addtask',component:AddTaskComponent},
+  {path:'listtask',component:ListTasksComponent},
+  {path:'edittask',component:EditTaskComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
